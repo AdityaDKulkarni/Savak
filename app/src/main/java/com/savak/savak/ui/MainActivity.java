d6de2ffc6e6a75f39b5e3c78f80ef16196489ded
AdityaDKulkarni <kulkarniaditya1997@gmail.com>
2018-12-05 13:13:09 +0530

    Parsed JSON response for GetSmartLibrary

--- app/src/main/java/com/savak/savak/ui/MainActivity.java
+++ app/src/main/java/com/savak/savak/ui/MainActivity.java
@@ -127,13 +127,6 @@ public class MainActivity extends AppCompatActivity {
 
                 Log.e("response", response);
 
-                /*JSONObject jsonObject = XML.toJSONObject(new String(response));
-                JSONObject soapEnv = jsonObject.optJSONObject("soap:Envelope");
-                JSONObject soapBody = soapEnv.optJSONObject("soap:Body");
-                JSONObject lib = soapBody.optJSONObject("GetSmartLibrariesResponse");
-
-
-                String result = lib.optString("GetSmartLibrariesResult");*/
                 jsonArray = new JSONArray(response);
 
                 for(int i = 0; i < jsonArray.length(); i++){
